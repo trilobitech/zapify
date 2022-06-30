2f9c9b4bf4e190c332559e320faedbb5ef60f59d
Pedro Silva <pdx.henrique@yahoo.com>
2022-06-29 22:08:45 -0300

    Remove unused code

---
+++ lib/app/app.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:zapfy/app/home/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return MaterialApp(
+      title: 'Appfy',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+        appBarTheme: theme.appBarTheme.copyWith(
+          foregroundColor: Colors.teal,
+          backgroundColor: Colors.white,
+          elevation: 0,
+          centerTitle: true,
+        ),
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: const MyHomePage(title: 'Zapfy'),
+    );
+  }
+}
