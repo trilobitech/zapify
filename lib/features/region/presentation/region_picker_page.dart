2943caa2b91aba23e7cd4276f50018732b42282d
Pedro Silva <pdx.henrique@yahoo.com>
2022-07-13 18:33:05 -0300

    Mark current region as selected in picker

--- lib/features/region/presentation/region_picker_page.dart
+++ lib/features/region/presentation/region_picker_page.dart
@@ -4,7 +4,9 @@ import 'package:zapfy/features/region/presentation/region_picker_controller.dart
 import 'package:zapfy/features/shared/domain/entity/region.dart';
 
 class RegionPicker extends StatefulWidget {
-  const RegionPicker({Key? key}) : super(key: key);
+  const RegionPicker({Key? key, this.selected}) : super(key: key);
+
+  final Region? selected;
 
   @override
   _RegionPickerState createState() => _RegionPickerState();
@@ -58,7 +60,10 @@ class _RegionPickerState extends State<RegionPicker> {
                   final region = _regions[i];
                   return InkWell(
                     onTap: () => Navigator.of(context).pop(region),
-                    child: _RegionListTile(region: region),
+                    child: _RegionListTile(
+                      region: region,
+                      isSelected: widget.selected == region,
+                    ),
                   );
                 },
               ),
@@ -74,25 +79,27 @@ class _RegionListTile extends StatelessWidget {
   const _RegionListTile({
     Key? key,
     required Region region,
+    this.isSelected = false,
   })  : _region = region,
         super(key: key);
 
   final Region _region;
+  final bool isSelected;
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return ListTile(
       visualDensity: VisualDensity.standard,
+      selected: isSelected,
+      selectedColor: Colors.white,
+      selectedTileColor: theme.colorScheme.primary,
       title: Row(
         children: [
           SizedBox(
             width: 50,
             child: Text(
-              _region.code,
-              style: const TextStyle(
-                color: Colors.grey,
-                fontWeight: FontWeight.bold,
-              ),
+              _region.flag ?? '',
             ),
           ),
           Text(_region.name),
