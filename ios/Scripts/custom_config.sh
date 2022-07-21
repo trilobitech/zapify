fef1854f4a5113d99b7fa66e6374785253ee91a8
Pedro Silva <pdx.henrique@yahoo.com>
2022-07-21 01:18:28 -0300

    Extract code from main.dart to respective native platforms
    - Fix iOS build

---
+++ ios/Scripts/custom_config.sh
@@ -0,0 +1,7 @@
+# https://stackoverflow.com/a/41020969
+# https://stackoverflow.com/a/32704702
+if [ ${CONFIGURATION} = "Release" ]; then
+    /usr/libexec/PlistBuddy -c "Set :FirebaseCrashlyticsCollectionEnabled YES" "${TARGET_BUILD_DIR}/${INFOPLIST_PATH}"
+    /usr/libexec/PlistBuddy -c "Set :firebase_performance_collection_enabled YES" "${TARGET_BUILD_DIR}/${INFOPLIST_PATH}"
+    /usr/libexec/PlistBuddy -c "Set :FIREBASE_ANALYTICS_COLLECTION_ENABLED YES" "${TARGET_BUILD_DIR}/${INFOPLIST_PATH}"
+fi
