ee573591ce4026279c9479eccbd67b21aad1780c
Pedro Silva <pdx.henrique@yahoo.com>
2022-07-23 19:39:14 -0300

    Add logDebug function

--- lib/core/logger.dart
+++ lib/core/logger.dart
@@ -1,8 +1,17 @@
+import 'package:flutter/foundation.dart';
 import 'package:zapfy/core/firebase.dart';
-
 import 'package:stack_trace/stack_trace.dart';
 
 typedef OnError<T> = T Function(Object exception, StackTrace? stack);
+
+void logDebug(dynamic message) {
+  if (!kDebugMode) return;
+
+  final location = Trace.current(1).frames[0].location;
+  debugPrint(
+    '🤖 \u001b[96m\u001b[1m$location\u001b[0m\u001b[39m: ${message.toString()}',
+  );
+}
 
 void logError(Object? error, [StackTrace? stackTrace]) {
   stackTrace ??= StackTrace.current;
