c944c2b474a770f25596f8c0591414061c268542
Pedro Silva <pdx.henrique@yahoo.com>
2022-07-25 12:32:52 -0300

    Update failure message implementation

--- lib/core/error_handler/failure_message_resolver.dart
+++ lib/core/error_handler/failure_message_resolver.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/widgets.dart';
 import 'package:zapify/core/error_handler/error_message_resolver.dart';
 import 'package:zapify/core/error_handler/failure.dart';
+import 'package:zapify/core/ext/context.dart';
 
 class FailureErrorMessageResolver extends ErrorMessageResolver {
   @override
   String? maybeResolve(BuildContext context, error) {
     if (error is Failure) {
-      return error.message(context);
+      return error.message(context.strings);
     }
     return null;
   }
