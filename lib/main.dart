807606b94800cf02730f0c0c94f9d43a02879522
Pedro Silva <pdx.henrique@yahoo.com>
2022-07-26 02:54:29 -0300

    Firebase zoeiro

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:zapify/app/modules.dart';
 import 'package:zapify/app/zapify_app.dart';
@@ -15,6 +16,15 @@ void main() {
       await Firebase.initializeApp(
         options: DefaultFirebaseOptions.currentPlatform,
       );
+
+      if (kDebugMode) {
+        // for some reason config set on native android not working well
+        await Future.wait([
+          crashlytics.setCrashlyticsCollectionEnabled(false),
+          performance.setPerformanceCollectionEnabled(false),
+          analytics.setAnalyticsCollectionEnabled(false),
+        ]);
+      }
 
       FlutterError.onError = crashlytics.recordFlutterFatalError;
 
