e37386f972baf9ef5ffce85b9a0fbb78279d1ee6
Pedro Silva <pdx.henrique@gmail.com>
2022-11-04 03:37:05 -0300

    Update build scripts

--- bin/get-dart-define.sh
+++ bin/get-dart-define.sh
@@ -2,6 +2,6 @@
 
 PROJECT_DIR=$(dirname $(dirname ${BASH_SOURCE[0]}))
 
-APP_NAME=$(grep 'name:' $PROJECT_DIR/pubspec.yaml | tail -n1 | awk '{ print $2}')
+APP_NAME=$(grep '^name:' $PROJECT_DIR/pubspec.yaml | tail -n1 | awk '{ print toupper($2)}')
 
 env | grep -ie "^${APP_NAME}_" | sed -e 's/^/--dart-define /'
