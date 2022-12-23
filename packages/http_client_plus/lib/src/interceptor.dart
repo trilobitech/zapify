2cd1bedaca43a6855ad455c545b24b62b86e1b57
Pedro Silva <pdx.henrique@gmail.com>
2022-12-23 19:37:14 -0300

    Replace http_client_plus from path mode to git

--- packages/http_client_plus/lib/src/interceptor.dart
+++
@@ -1,7 +0,0 @@
-import 'package:http/http.dart';
-
-typedef Next = Future<StreamedResponse> Function(BaseRequest request);
-
-abstract class HttpInterceptor {
-  Future<StreamedResponse> intercept(BaseRequest request, Next next);
-}
