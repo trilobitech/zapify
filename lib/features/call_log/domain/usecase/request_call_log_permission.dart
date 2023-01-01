a6858b33a8cd88cb393eb9ef737e2241bb9dfc0a
Pedro Silva <pdx.henrique@yahoo.com>
2023-01-01 20:04:07 -0300

    Handle permission permanetly denied
    - try to open settings

--- lib/features/call_log/domain/usecase/request_call_log_permission.dart
+++ lib/features/call_log/domain/usecase/request_call_log_permission.dart
@@ -2,6 +2,11 @@ import 'package:permission_handler/permission_handler.dart';
 
 class RequestCallLogPermissionUseCase {
   Future<void> call() async {
-    await Permission.phone.request();
+    final status = await Permission.phone.status;
+    if (status.isPermanentlyDenied) {
+      await openAppSettings();
+    } else if (!status.isGranted) {
+      await Permission.phone.request();
+    }
   }
 }
