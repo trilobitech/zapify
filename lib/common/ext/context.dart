ca35412a78a9c045552425900b0253b0746a7e9a
Pedro Silva <9375141+pedrox-hs@users.noreply.github.com>
2025-03-29 20:53:34 -0300

    Disable synthetic-package for app localizations

--- lib/common/ext/context.dart
+++ lib/common/ext/context.dart
@@ -1,4 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+import '../resources/localizations.dart';
+
 export 'package:provider/provider.dart' show ReadContext;
 
-export 'l10n.dart';
+export '../resources/localizations.dart';
 export 'string.dart';
+
+extension StringsContext on BuildContext {
+  AppLocalizations get strings => AppLocalizations.of(this);
+}
