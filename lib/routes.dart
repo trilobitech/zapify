2c207a933b779e9cca82cdf4f040dd3c5b421f77
Pedro Silva <9375141+pedrox-hs@users.noreply.github.com>
2025-06-02 20:06:22 -0300

    feat(navigation): Add extension `getFromData`
    - Useful to get values from route extras

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:simple_nav/simple_nav.dart';
 
+import 'common/ext/navigator.dart';
 import 'features/chat_app/presentation/chat_app_page.dart';
 import 'features/home/main/home_page.dart';
 import 'features/region/presentation/region_picker_page.dart';
@@ -27,7 +28,8 @@ final routes = <String, RouteBuilder>{
     return MaterialPageRoute(
       fullscreenDialog: true,
       settings: const RouteSettings(name: 'RegionPicker'),
-      builder: (_) => RegionPicker(selected: _getSelectedCode(extras.data)),
+      builder:
+          (_) => RegionPicker(selected: extras.getFromData('selected_code')),
     );
   },
 
@@ -45,6 +47,3 @@ final routes = <String, RouteBuilder>{
     );
   },
 };
-
-String? _getSelectedCode(dynamic data) =>
-    data is Map<String, dynamic> ? data['selected_code'] : null;
